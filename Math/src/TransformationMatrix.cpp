790ff0673e8226291f89557d5ad61e02737df5c8
Maksym Bas <maksym.bas@materialise.kiev.ua>
2019-04-22 18:41:09 +0300

    Small performance improvements in transformation matrix

--- Math/src/TransformationMatrix.cpp
+++ Math/src/TransformationMatrix.cpp
@@ -10,8 +10,10 @@ TransformationMatrix::TransformationMatrix()
 
 TransformationMatrix::TransformationMatrix(double* i_from16)
   {
-  for (ushort i = 0; i < 16; ++i)
-    m_matrix[i / 4][i % 4] = i_from16[i];
+  m_matrix[0][0] = i_from16[0];   m_matrix[0][1] = i_from16[1];   m_matrix[0][2] = i_from16[2];   m_matrix[0][3] = i_from16[3];
+  m_matrix[1][0] = i_from16[4];   m_matrix[1][1] = i_from16[5];   m_matrix[1][2] = i_from16[6];   m_matrix[1][3] = i_from16[7];
+  m_matrix[2][0] = i_from16[8];   m_matrix[2][1] = i_from16[9];   m_matrix[2][2] = i_from16[10];  m_matrix[2][3] = i_from16[11];
+  m_matrix[3][0] = i_from16[12];  m_matrix[3][1] = i_from16[13];  m_matrix[3][2] = i_from16[14];  m_matrix[3][3] = i_from16[15];
   m_is_identity = _CheckIdentity();
   }
 
@@ -33,19 +35,24 @@ TransformationMatrix::TransformationMatrix(const TransformationMatrix& i_other)
 
 void TransformationMatrix::SetIdentity()
   {
-  for (ushort i = 0u; i < 16; ++i)
-    {
-    m_matrix[i / 4][i % 4] = !(i % 5);
-    }
+  SetDiagonal(1);
   m_is_identity = true;
   }
 
 void TransformationMatrix::SetDiagonal(double i_number)
   {
-  for (ushort i = 0u; i < 16; ++i)
-    {
-    m_matrix[i / 4][i % 4] = i_number * !(i % 5);
-    }
+  m_matrix[0][0] = i_number;  m_matrix[0][1] = 0;         m_matrix[0][2] = 0;         m_matrix[0][3] = 0;
+  m_matrix[1][0] = 0;         m_matrix[1][1] = i_number;  m_matrix[1][2] = 0;         m_matrix[1][3] = 0;
+  m_matrix[2][0] = 0;         m_matrix[2][1] = 0;         m_matrix[2][2] = i_number;  m_matrix[2][3] = 0;
+  m_matrix[3][0] = 0;         m_matrix[3][1] = 0;         m_matrix[3][2] = 0;         m_matrix[3][3] = i_number;
+
+  if(i_number!=1)
+  {
+  m_inverse_matrix[0][0] = 1. / i_number;   m_inverse_matrix[0][1] = 0;               m_inverse_matrix[0][2] = 0;               m_inverse_matrix[0][3] = 0;
+  m_inverse_matrix[1][0] = 0;               m_inverse_matrix[1][1] = 1. / i_number;   m_inverse_matrix[1][2] = 0;               m_inverse_matrix[1][3] = 0;
+  m_inverse_matrix[2][0] = 0;               m_inverse_matrix[2][1] = 0;               m_inverse_matrix[2][2] = 1. / i_number;   m_inverse_matrix[2][3] = 0;
+  m_inverse_matrix[3][0] = 0;               m_inverse_matrix[3][1] = 0;               m_inverse_matrix[3][2] = 0;               m_inverse_matrix[3][3] = 1. / i_number;
+  }
   m_is_identity = false;
   }
 
