1142a675788f66c7ade7b99a4e74bafdffe0a416
basicula <maksim.basum@gmail.com>
2020-08-10 21:24:16 +0300

    Update cmake for thirdparties to same style

--- cmake/OpenCL.cmake
+++ cmake/OpenCL.cmake
@@ -1,6 +1,6 @@
 find_package(OpenCL QUIET)
 
-if(NOT GLUT_FOUND)
+if(NOT OpenCL_FOUND)
 	message("Installing OpenCL...")
 	
 	set(OpenCLPath "${THIRD_PARTIES}/OpenCL")
@@ -8,11 +8,13 @@ if(NOT GLUT_FOUND)
 	set(OPENCL_INCLUDE_DIRS "${OpenCLPath}/include" PARENT_SCOPE)
 	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
 		set(OPENCL_LIBRARY_DIRS "${OpenCLPath}/lib/x64" PARENT_SCOPE)
+		set(OPENCL_LIBRARIES OpenCL.lib PARENT_SCOPE)
 		#file(	COPY "${OpenCLPath}/bin/x64/.dll"
 		#	DESTINATION "${CMAKE_BINARY_DIR}"
 		#)
 	else()
 		set(OPENCL_LIBRARY_DIRS "${OpenCLPath}/lib/x32" PARENT_SCOPE)
+    set(OPENCL_LIBRARIES OpenCL.lib PARENT_SCOPE)
 		#file(	COPY "${OpenCLPath}/bin/.dll"
 		#	DESTINATION "${CMAKE_BINARY_DIR}"
 		#)
--- cmake/glut.cmake
+++ cmake/glut.cmake
@@ -7,13 +7,15 @@ if(NOT GLUT_FOUND)
 	set(GLUT_INCLUDE_DIRS "${FreeglutPath}/include" PARENT_SCOPE)
 	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
 		set(GLUT_LIBRARY_DIRS "${FreeglutPath}/lib/x64" PARENT_SCOPE)
+		set(GLUT_LIBRARIES freeglut.lib PARENT_SCOPE)
 		file(	COPY "${FreeglutPath}/bin/x64/freeglut.dll"
-			DESTINATION "${CMAKE_BINARY_DIR}"
+			DESTINATION "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}"
 		)
 	else()
 		set(GLUT_LIBRARY_DIRS "${FreeglutPath}/lib" PARENT_SCOPE)
-		file(	COPY "${FreeglutPath}/bin/freeglut.dll"
-			DESTINATION "${CMAKE_BINARY_DIR}"
+    set(GLUT_LIBRARIES freeglut.lib PARENT_SCOPE)
+		file(	COPY "${FreeglutPath}/bin/x32/freeglut.dll"
+			DESTINATION "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}"
 		)
 	endif()
 endif()
