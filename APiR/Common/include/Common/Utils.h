715a13e0b358b33d9fc4536d6d509d193038411f
basicula <maksim.basum@gmail.com>
2021-05-09 15:51:14 +0300

    Add extended min max functions for device code execution

---
+++ APiR/Common/include/Common/Utils.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <Macros.h>
+#ifdef __CUDA_ARCH__
+#include <cuda_runtime.h>
+#else
+#include <algorithm>
+#endif
+
+namespace Utils {
+  template<class T>
+  HOSTDEVICE const T& min(const T& i_first, const T& i_second) {
+#ifdef __CUDA_ARCH__
+    return i_first < i_second ? i_first : i_second;
+#else
+    return std::min(i_first, i_second);
+#endif
+    }
+
+  template<class T>
+  HOSTDEVICE const T& max(const T& i_first, const T& i_second) {
+#ifdef __CUDA_ARCH__
+    return i_first > i_second ? i_first : i_second;
+#else
+    return std::max(i_first, i_second);
+#endif
+    }
+  }
