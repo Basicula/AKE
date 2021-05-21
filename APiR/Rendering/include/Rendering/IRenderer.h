4350e4e564991c9f7237bdccf7b70396cedefdb9
basicula <maksim.basum@gmail.com>
2021-05-21 21:03:42 +0300

    Update interface for renderer

--- APiR/Rendering/include/Rendering/IRenderer.h
+++ APiR/Rendering/include/Rendering/IRenderer.h
@@ -3,21 +3,27 @@
 
 #include <Rendering/Scene.h>
 
+#include <memory>
+
 class IRenderer
   {
   public:
 
     void SetOutputImage(Image* iop_image);
+    void SetScene(const Scene* ip_scene);
 
-    virtual void Render(const Scene& m_scene) = 0;
+    virtual void Render() = 0;
 
     std::size_t GetRenderingDepth() const;
     void SetRenderingDepth(std::size_t i_depth);
 
   protected:
+    // post processing function for derived classes
     virtual void _OutputImageWasSet() = 0;
+    virtual void _SceneWasSet() = 0;
 
   protected:
     size_t m_depth;
     Image* mp_frame_image;
+    const Scene* mp_scene;
   };
--- APiR/Rendering/src/CPURayTracer.cpp
+++ APiR/Rendering/src/CPURayTracer.cpp
@@ -6,9 +6,7 @@ CPURayTracer::CPURayTracer()
   : m_active_camera(nullptr){
   }
 
-void CPURayTracer::Render(const Scene& i_scene) {
-  if (m_active_camera == nullptr || &i_scene.GetActiveCamera() != m_active_camera)
-    _UpdateRaysForActiveCamera(i_scene);
+void CPURayTracer::Render() {
   //for(auto i_pixel_id = 0; i_pixel_id < mp_frame_image->GetSize(); ++i_pixel_id)
   ThreadPool::GetInstance()->ParallelFor(
     static_cast<std::size_t>(0),
@@ -17,7 +15,7 @@ void CPURayTracer::Render(const Scene& i_scene) {
     {
     auto x = i_pixel_id % mp_frame_image->GetWidth();
     auto y = i_pixel_id / mp_frame_image->GetWidth();
-    mp_frame_image->SetPixel(x, y, _TraceRay(i_scene, i_pixel_id));
+    mp_frame_image->SetPixel(x, y, _TraceRay(i_pixel_id));
     }
   );
   }
@@ -27,13 +25,13 @@ void CPURayTracer::_OutputImageWasSet() {
   m_intersection_records.resize(mp_frame_image->GetSize());
   }
 
-void CPURayTracer::_UpdateRaysForActiveCamera(const Scene& i_scene)   {
+void CPURayTracer::_SceneWasSet() {
   m_rays.clear();
-  m_active_camera = &i_scene.GetActiveCamera();
+  m_active_camera = &mp_scene->GetActiveCamera();
 
   const auto& ray_origin = m_active_camera->GetLocation();
   for (auto y = 0.0; y < mp_frame_image->GetHeight(); ++y)
-    for (auto x = 0.0; x < mp_frame_image->GetWidth(); ++x)     {
+    for (auto x = 0.0; x < mp_frame_image->GetWidth(); ++x) {
       const auto& ray_dir = m_active_camera->GetDirection(
         x / mp_frame_image->GetWidth(),
         y / mp_frame_image->GetHeight());
@@ -41,37 +39,35 @@ void CPURayTracer::_UpdateRaysForActiveCamera(const Scene& i_scene)   {
       }
   }
 
-Color CPURayTracer::_TraceRay(const Scene& i_scene, std::size_t i_ray_id)   {
+Color CPURayTracer::_TraceRay(std::size_t i_ray_id)   {
   auto& hit = m_intersection_records[i_ray_id];
   const auto& camera_ray = m_rays[i_ray_id];
 
   hit.Reset();
-  bool intersected = i_scene.TraceRay(hit, camera_ray);
+  bool intersected = mp_scene->TraceRay(hit, camera_ray);
 
   if (!intersected || !hit.m_material)
-    return i_scene.GetBackGroundColor();
+    return mp_scene->GetBackGroundColor();
 
-  return _ProcessIntersection(i_scene, hit, camera_ray);
+  return _ProcessIntersection(hit, camera_ray);
   }
 
 Color CPURayTracer::_ProcessIntersection(
-  const Scene& i_scene,
   const IntersectionRecord& i_intersection,
   const Ray& i_camera_ray)   {
   Color reflected_color;
   if (i_intersection.m_material->IsReflectable())
-    reflected_color = _ProcessReflection(i_scene, i_intersection, i_camera_ray);
+    reflected_color = _ProcessReflection(i_intersection, i_camera_ray);
 
   Color refracted_color;
   if (i_intersection.m_material->IsRefractable())
-    refracted_color = _ProcessRefraction(i_scene, i_intersection, i_camera_ray);
+    refracted_color = _ProcessRefraction(i_intersection, i_camera_ray);
 
-  Color light_influence = _ProcessLightInfluence(i_scene, i_intersection, i_camera_ray);
+  Color light_influence = _ProcessLightInfluence(i_intersection, i_camera_ray);
   return light_influence + reflected_color;
   }
 
 Color CPURayTracer::_ProcessReflection(
-  const Scene& i_scene,
   const IntersectionRecord& i_intersection,
   const Ray& i_camera_ray)   {
   std::size_t depth = 0;
@@ -83,16 +79,15 @@ Color CPURayTracer::_ProcessReflection(
     local_ray.SetOrigin(local_record.m_intersection);
     local_ray.SetDirection(reflected_direction);
     local_record.Reset();
-    if (i_scene.TraceRay(local_record, local_ray))
+    if (mp_scene->TraceRay(local_record, local_ray))
       ++depth;
     else
-      return i_scene.GetBackGroundColor();
+      return mp_scene->GetBackGroundColor();
     }
-  return _ProcessLightInfluence(i_scene, local_record, i_camera_ray) * i_intersection.m_material->ReflectionInfluence();
+  return _ProcessLightInfluence(local_record, i_camera_ray) * i_intersection.m_material->ReflectionInfluence();
   }
 
 Color CPURayTracer::_ProcessRefraction(
-  const Scene& /*i_scene*/,
   const IntersectionRecord& /*i_intersection*/,
   const Ray& /*i_camera_ray*/)   {
   //double refraction_coef = i_ray.GetEnvironment() / object->GetMaterial().GetRefraction();
@@ -110,7 +105,6 @@ Color CPURayTracer::_ProcessRefraction(
   }
 
 Color CPURayTracer::_ProcessLightInfluence(
-  const Scene& i_scene,
   const IntersectionRecord& i_intersection,
   const Ray& i_camera_ray)   {
   const auto& view_direction = i_camera_ray.GetDirection();
@@ -118,15 +112,15 @@ Color CPURayTracer::_ProcessLightInfluence(
   std::size_t red, green, blue, active_lights_cnt;
   red = green = blue = active_lights_cnt = 0;
   Ray to_light(i_intersection.m_intersection + i_intersection.m_normal * 1e-10, Vector3d(0, 1, 0));
-  for (auto i = 0u; i < i_scene.GetNumLights(); ++i)     {
-    const auto& light = i_scene.GetLight(i);
+  for (auto i = 0u; i < mp_scene->GetNumLights(); ++i)     {
+    const auto& light = mp_scene->GetLight(i);
     if (!light->GetState())
       continue;
     ++active_lights_cnt;
     const auto light_direction = light->GetDirection(i_intersection.m_intersection);
     to_light.SetDirection(-light_direction);
     IntersectionRecord temp_intersection;
-    const bool is_intersected = i_scene.TraceRay(temp_intersection, to_light);
+    const bool is_intersected = mp_scene->TraceRay(temp_intersection, to_light);
     if (!is_intersected || light_direction.Dot(light->GetDirection(temp_intersection.m_intersection)) < 0.0)       {
       Color light_influence =
         i_intersection.m_material->GetLightInfluence(
