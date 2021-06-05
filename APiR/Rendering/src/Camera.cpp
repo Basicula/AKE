4f4e4cd352440f805390014721479195a9abbba1
basicula <maksim.basum@gmail.com>
2021-06-05 15:25:59 +0300

    Update camera interface

--- APiR/Rendering/src/Camera.cpp
+++ APiR/Rendering/src/Camera.cpp
@@ -1,7 +1,6 @@
 #include <Rendering/Camera.h>
 #include <Math/Constants.h>
-
-#include <cmath>
+#include <Geometry/Transformation.h>
 
 Camera::Camera(
     const Vector3d& i_lookFrom
@@ -17,15 +16,19 @@ Camera::Camera(
   , m_aspect(i_aspect)
   , m_focusDistance(i_focusDist)
   {
+  _Init();
+  }
+
+void Camera::_Init() {
   const double theta = m_fov * PI / 180;
-  const double halfHeight = std::tan(theta / 2);
+  const double halfHeight = tan(theta / 2);
   const double halfWidth = halfHeight * m_aspect;
 
   m_right = m_up.CrossProduct(m_direction).Normalized();
   m_corner = m_right * halfWidth + m_up * halfHeight - m_direction * m_focusDistance;
   m_u = m_right * halfWidth * 2;
   m_v = m_up * halfHeight * 2;
-  }
+}
 
 const Vector3d& Camera::GetDirection() const {
   return m_direction;
@@ -37,4 +40,16 @@ const Vector3d& Camera::GetUpVector() const {
 
 const Vector3d& Camera::GetRight() const {
   return m_right;
+}
+
+void Camera::Move(const Vector3d& i_displacement_vector) {
+  m_location += i_displacement_vector;
+}
+
+void Camera::Rotate(const Vector3d& i_rotation_axis, const double i_angle_in_rad) {
+  Transformation rotation;
+  rotation.SetRotation(i_rotation_axis, i_angle_in_rad);
+  rotation.Rotate(m_direction);
+  rotation.Rotate(m_up);
+  _Init();
 }
