ad39a04b5543e4a875e5640f2145b3e5ba67e49c
basicula <maksim.basum@gmail.com>
2021-06-13 16:01:03 +0300

    Simple camera implementation

---
+++ APiR/Rendering/include/Rendering/SimpleCamera.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <Rendering/Camera.h>
+
+class SimpleCamera : public Camera {
+public:
+  // fov in degrees
+  // aspect = width / height
+  SimpleCamera(
+    const Vector3d& i_location,
+    const Vector3d& i_direction,
+    const Vector3d& i_up_vector,
+    const double i_fov,
+    const double i_aspect);
+
+  HOSTDEVICE virtual Ray CameraRay(const double i_u, const double i_v) const override;
+
+protected:
+  virtual void _Update() override;
+
+private:
+  Vector3d m_corner;
+
+  double m_fov;
+  double m_aspect;
+
+  double m_view_angle;
+  double m_half_width;
+  double m_half_height;
+
+};
---
+++ APiR/Rendering/src/SimpleCamera.cpp
@@ -0,0 +1,28 @@
+#include <Rendering/SimpleCamera.h>
+
+#include <Math/Constants.h>
+
+SimpleCamera::SimpleCamera(
+  const Vector3d& i_location,
+  const Vector3d& i_direction,
+  const Vector3d& i_up_vector,
+  const double i_fov,
+  const double i_aspect)
+  : Camera(i_location,
+    i_direction,
+    i_up_vector)
+  , m_fov(i_fov)
+  , m_aspect(i_aspect) {
+  m_view_angle = m_fov * PI / 180;
+  m_half_height = tan(m_view_angle / 2);
+  m_half_width = m_half_height * m_aspect;
+}
+
+Ray SimpleCamera::CameraRay(const double i_u, const double i_v) const {
+  return { m_location, (m_right * i_u * m_half_width * 2 + m_up * i_v * m_half_height * 2 - m_corner).Normalized() };
+}
+
+void SimpleCamera::_Update() {
+  m_right = m_up.CrossProduct(m_direction).Normalized();
+  m_corner = m_right * m_half_width + m_up * m_half_height - m_direction;
+}
