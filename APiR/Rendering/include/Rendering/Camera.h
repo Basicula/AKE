1087b06c41b84f2c34cadc685c8d32db65e61bac
basicula <maksim.basum@gmail.com>
2021-06-13 16:00:28 +0300

    Create Camera abstraction

--- APiR/Rendering/include/Rendering/Camera.h
+++ APiR/Rendering/include/Rendering/Camera.h
@@ -3,21 +3,14 @@
 
 #include <Math/Vector.h>
 
-#include <string>
-
 class Camera
   {
   public:
-    // fov in degrees
-    // aspect = width / height
-    // focusDist - distance from "camera" to generated(rendered) picture
     Camera(
-      const Vector3d& i_lookFrom,
-      const Vector3d& i_lookAt,
-      const Vector3d& i_up,
-      double i_fov,
-      double i_aspect,
-      double i_focusDist);
+      const Vector3d& i_location,
+      const Vector3d& i_direction,
+      const Vector3d& i_up_vector);
+    virtual ~Camera() = default;
 
     HOSTDEVICE const Vector3d& GetLocation() const;
     void SetLocation(const Vector3d& i_location);
@@ -31,51 +24,15 @@ class Camera
     void Move(const Vector3d& i_displacement_vector);
     void Rotate(const Vector3d& i_rotation_axis, const double i_angle_in_rad);
 
-    HOSTDEVICE Ray CameraRay(double i_u, double i_v) const;
+    HOSTDEVICE virtual Ray CameraRay(const double i_u, const double i_v) const = 0;
 
-    std::string Serialize() const;
+  protected:
+    // Updates derivated class specific data after some camera base manipulations
+    virtual void _Update() = 0;
 
-  private:
-    void _Init();
-
-  private:
+  protected:
     Vector3d m_location;
     Vector3d m_direction;
     Vector3d m_up;
     Vector3d m_right;
-    Vector3d m_corner;
-    Vector3d m_u;
-    Vector3d m_v;
-
-    double m_fov;
-    double m_aspect;
-    double m_focusDistance;
   };
-
-inline const Vector3d& Camera::GetLocation() const
-  {
-  return m_location;
-  }
-
-inline void Camera::SetLocation(const Vector3d& i_location)
-  {
-  m_location = i_location;
-  }
-
-inline Ray Camera::CameraRay(double i_u, double i_v) const
-  {
-  return { m_location, (m_u * i_u + m_v * i_v - m_corner).Normalized() };
-  }
-
-inline std::string Camera::Serialize() const
-  {
-  std::string res = "{ \"Camera\" : { ";
-  res += " \"Location\" : " + m_location.Serialize() + " ,";
-  res += " \"LookAt\" : " + (m_location + m_direction).Serialize() + " ,";
-  res += " \"Up\" : " + m_up.Serialize() + " ,";
-  res += " \"FoV\" : " + std::to_string(m_fov) + " ,";
-  res += " \"Aspect\" : " + std::to_string(m_aspect) + " ,";
-  res += " \"FocusDistance\" : " + std::to_string(m_focusDistance);
-  res += " } }";
-  return res;
-  }
--- APiR/Rendering/src/Camera.cpp
+++ APiR/Rendering/src/Camera.cpp
@@ -3,32 +3,14 @@
 #include <Geometry/Transformation.h>
 
 Camera::Camera(
-    const Vector3d& i_lookFrom
-  , const Vector3d& i_lookAt
-  , const Vector3d& i_up
-  , double i_fov
-  , double i_aspect
-  , double i_focusDist)
-  : m_location(i_lookFrom)
-  , m_direction((i_lookAt - i_lookFrom).Normalized())
-  , m_up(i_up.Normalized())
-  , m_fov(i_fov)
-  , m_aspect(i_aspect)
-  , m_focusDistance(i_focusDist)
+    const Vector3d& i_location
+  , const Vector3d& i_direction
+  , const Vector3d& i_up_vector)
+  : m_location(i_location)
+  , m_direction(i_direction.Normalized())
+  , m_up(i_up_vector.Normalized())
   {
-  _Init();
   }
-
-void Camera::_Init() {
-  const double theta = m_fov * PI / 180;
-  const double halfHeight = tan(theta / 2);
-  const double halfWidth = halfHeight * m_aspect;
-
-  m_right = m_up.CrossProduct(m_direction).Normalized();
-  m_corner = m_right * halfWidth + m_up * halfHeight - m_direction * m_focusDistance;
-  m_u = m_right * halfWidth * 2;
-  m_v = m_up * halfHeight * 2;
-}
 
 const Vector3d& Camera::GetDirection() const {
   return m_direction;
@@ -44,6 +26,7 @@ const Vector3d& Camera::GetRight() const {
 
 void Camera::Move(const Vector3d& i_displacement_vector) {
   m_location += i_displacement_vector;
+  _Update();
 }
 
 void Camera::Rotate(const Vector3d& i_rotation_axis, const double i_angle_in_rad) {
@@ -51,5 +34,13 @@ void Camera::Rotate(const Vector3d& i_rotation_axis, const double i_angle_in_rad
   rotation.SetRotation(i_rotation_axis, i_angle_in_rad);
   rotation.Rotate(m_direction);
   rotation.Rotate(m_up);
-  _Init();
+  _Update();
+}
+
+const Vector3d& Camera::GetLocation() const {
+  return m_location;
+}
+
+void Camera::SetLocation(const Vector3d& i_location) {
+  m_location = i_location;
 }
