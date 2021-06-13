c70d84f865e66410b381422d7a9787b21ab2f460
basicula <maksim.basum@gmail.com>
2021-06-13 17:29:34 +0300

    restructure project

---
+++ ThirdParties/ThirdParties.cmake
@@ -0,0 +1,20 @@
+find_package(OpenGL REQUIRED)
+
+# NOTE: need to add ${THIRD_PARTIES} prefix for all pathes due to CMake include behavior
+
+include("${THIRD_PARTIES}/glut.cmake")
+include("${THIRD_PARTIES}/glfw.cmake")
+include("${THIRD_PARTIES}/gtest.cmake")
+if (ENABLE_OPENCL)
+  add_definitions(-DENABLED_OPENCL)
+  include("${THIRD_PARTIES}/OpenCL.cmake")
+endif()
+if (ENABLE_CUDA)
+  add_definitions(-DENABLED_CUDA)
+  include("${THIRD_PARTIES}/CUDA.cmake")
+endif()
+
+if (ENABLE_PYTHON_WRAPPER)
+# for python integration
+add_subdirectory(${THIRD_PARTIES}/pybind11)
+endif()
