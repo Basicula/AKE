133b6988c8721844d29fbc18be920c8a531b25f6
basicula <maksim.basum@gmail.com>
2021-09-05 12:46:29 +0300

    Update thirdparties with imgui support

--- ThirdParties/glfw.cmake
+++ ThirdParties/glfw.cmake
@@ -1,9 +1,7 @@
 include(FetchContent)
 
-if (NOT TARGET glfw)
-    FetchContent_Declare(glfw
-    GIT_REPOSITORY "https://github.com/glfw/glfw"
-    GIT_TAG "3.3.3"
-    GIT_SHALLOW ON)
-    FetchContent_MakeAvailable(glfw)
-endif()
+FetchContent_Declare(glfw
+GIT_REPOSITORY "https://github.com/glfw/glfw"
+GIT_TAG "3.3.3"
+GIT_SHALLOW ON)
+FetchContent_MakeAvailable(glfw)
