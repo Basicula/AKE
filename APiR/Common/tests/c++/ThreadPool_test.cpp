8ebf6ff4127d87573557afcd76d1688f37fcefd9
Basicula <maksim.basum@gmail.com>
2022-04-03 20:39:43 +0300

    Clean up Common.Tests

--- APiR/Common/tests/c++/ThreadPool_test.cpp
+++ APiR/Common/tests/c++/ThreadPool_test.cpp
@@ -1,30 +1,40 @@
 #include "Common/ThreadPool.h"
-#include "Utils.h"
 
 #include <chrono>
 #include <gtest/gtest.h>
 #include <numeric>
 
+namespace {
+  using FunctionWrapperForTime = std::function<void()>;
+  std::size_t GetFunctionExecutionTime(const FunctionWrapperForTime& i_func, const std::size_t i_iterations)
+  {
+    const auto start = std::chrono::system_clock::now();
+    for (auto i = 0u; i < i_iterations; ++i)
+      i_func();
+    const auto end = std::chrono::system_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+  };
+
+}
+
 TEST(ThreadPoolTest, ThreadPoolCommonScenario)
 {
-  const std::size_t n = 1000000;
-  const std::size_t iterations = 1;
+  constexpr std::size_t n = 1000000;
+  constexpr std::size_t iterations = 1;
 
-  std::vector<int> a(n);
-  std::iota(a.begin(), a.end(), 0);
-  auto sum_func = [&a](std::size_t i_start, std::size_t i_end) {
+  auto sum_func = [](const std::size_t i_start, const std::size_t i_end) {
     long long sum = 0;
     for (auto i = i_start; i < i_end; ++i)
       sum += 2;
     return sum;
   };
 
-  ThreadPool* pool = ThreadPool::GetInstance();
+  Parallel::ThreadPool* pool = Parallel::ThreadPool::GetInstance();
 
-  std::vector<long long> time_results;
-  for (auto k : { 1, 2, 4, 8 }) {
+  std::vector<std::size_t> time_results;
+  for (std::size_t k : { 1, 2, 4, 8 }) {
     const std::size_t interval_size = n / k;
-    auto time = function_wrapper_for_time(
+    auto time = GetFunctionExecutionTime(
       [&]() {
         std::vector<std::future<long long>> results(k);
         for (std::size_t i = 0; i < k; ++i) {
@@ -38,8 +48,8 @@ TEST(ThreadPoolTest, ThreadPoolCommonScenario)
     time_results.push_back(time);
   }
 
-  for (int i = 0; i < time_results.size(); ++i)
-    for (int j = i + 1; j < time_results.size(); ++j)
+  for (std::size_t i = 0; i < time_results.size(); ++i)
+    for (std::size_t j = i + 1; j < time_results.size(); ++j)
       EXPECT_TRUE(time_results[i] > time_results[j]);
 }
 
@@ -47,17 +57,17 @@ TEST(ThreadPoolTest, ThreadPoolParallelFor)
 {
   srand(123);
 
-  const std::size_t iterations = 1;
-  const std::size_t n = 10;
+  constexpr std::size_t iterations = 1;
+  constexpr std::size_t n = 10;
 
   std::vector<std::vector<int>> matrix(n, std::vector<int>(n));
   for (auto& row : matrix)
     for (auto& elem : row)
       elem = rand() % 10;
 
-  ThreadPool* pool = ThreadPool::GetInstance();
+  Parallel::ThreadPool* pool = Parallel::ThreadPool::GetInstance();
 
-  auto common_time = function_wrapper_for_time(
+  const auto common_time = GetFunctionExecutionTime(
     [&]() {
       for (const auto& row : matrix)
         for (const auto& elem : row)
@@ -65,7 +75,7 @@ TEST(ThreadPoolTest, ThreadPoolParallelFor)
     },
     iterations);
 
-  auto parallel_for_time = function_wrapper_for_time(
+  const auto parallel_for_time = GetFunctionExecutionTime(
     [&]() {
       pool->ParallelFor(static_cast<std::size_t>(0), n, [&](std::size_t i) {
         for (const auto& elem : matrix[i])
@@ -74,7 +84,7 @@ TEST(ThreadPoolTest, ThreadPoolParallelFor)
     },
     iterations);
 
-  auto parallel_for_in_parallel_for_time = function_wrapper_for_time(
+  const auto parallel_for_in_parallel_for_time = GetFunctionExecutionTime(
     [&]() {
       pool->ParallelFor(static_cast<std::size_t>(0), n, [&](std::size_t i) {
         pool->ParallelFor(static_cast<std::size_t>(0), n, [&](std::size_t j) {
