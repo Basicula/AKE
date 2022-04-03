8e0d41660cb31e5959292ab7d32b035a04fc44bf
Basicula <maksim.basum@gmail.com>
2022-04-03 21:32:25 +0300

    Fix some warnings - Image

--- APiR/Image/include/Image/Image.h
+++ APiR/Image/include/Image/Image.h
@@ -9,7 +9,7 @@ public:
   Image(std::size_t i_width, std::size_t i_height, std::uint32_t i_default_color = 0);
   ~Image();
 
-  std::uint32_t GetPixel(std::size_t i_x, std::size_t i_y) const;
+  [[nodiscard]] std::uint32_t GetPixel(std::size_t i_x, std::size_t i_y) const;
   void GetPixelRGBA(std::size_t i_x,
                     std::size_t i_y,
                     uint8_t& o_red,
@@ -24,17 +24,17 @@ public:
                     uint8_t i_blue,
                     uint8_t i_alpha = 255);
 
-  HOSTDEVICE std::size_t GetWidth() const;
-  HOSTDEVICE std::size_t GetHeight() const;
-  std::size_t GetDepth() const;
-  std::size_t GetSize() const;
-  std::size_t GetBytesCount() const;
+  HOSTDEVICE [[nodiscard]] std::size_t GetWidth() const;
+  HOSTDEVICE [[nodiscard]] std::size_t GetHeight() const;
+  [[nodiscard]] std::size_t GetDepth() const;
+  [[nodiscard]] std::size_t GetSize() const;
+  [[nodiscard]] std::size_t GetBytesCount() const;
 
-  std::uint32_t* GetData() const;
-  uint8_t* GetRGBAData() const;
+  [[nodiscard]] std::uint32_t* GetData() const;
+  [[nodiscard]] uint8_t* GetRGBAData() const;
 
 private:
-  HOSTDEVICE std::size_t _ID(std::size_t i_x, std::size_t i_y) const;
+  HOSTDEVICE [[nodiscard]] std::size_t _ID(std::size_t i_x, std::size_t i_y) const;
 
 private:
   std::size_t m_width;
@@ -42,64 +42,5 @@ private:
   std::size_t m_size;
   MemoryManager::pointer<std::uint32_t> m_pixels;
 
-  static const unsigned char m_bytes_per_pixel = 4;
+  static constexpr unsigned char m_bytes_per_pixel = 4;
 };
-
-inline std::size_t Image::_ID(std::size_t i_x, std::size_t i_y) const
-{
-  return i_y * m_width + i_x;
-}
-
-inline std::uint32_t Image::GetPixel(std::size_t i_x, std::size_t i_y) const
-{
-  return m_pixels.mp_data[_ID(i_x, i_y)];
-}
-
-inline void Image::GetPixelRGBA(std::size_t i_x,
-                                std::size_t i_y,
-                                uint8_t& o_red,
-                                uint8_t& o_green,
-                                uint8_t& o_blue,
-                                uint8_t& o_alpha) const
-{
-  auto rgba_data = reinterpret_cast<const uint8_t*>(&m_pixels.mp_data[_ID(i_x, i_y)]);
-  o_red = rgba_data[0];
-  o_green = rgba_data[1];
-  o_blue = rgba_data[2];
-  o_alpha = rgba_data[3];
-}
-
-inline void Image::SetPixel(std::size_t i_x, std::size_t i_y, std::uint32_t i_color)
-{
-  m_pixels.mp_data[_ID(i_x, i_y)] = i_color;
-}
-
-inline std::size_t Image::GetWidth() const
-{
-  return m_width;
-}
-
-inline std::size_t Image::GetHeight() const
-{
-  return m_height;
-}
-
-inline std::size_t Image::GetSize() const
-{
-  return m_size;
-}
-
-inline std::size_t Image::GetBytesCount() const
-{
-  return GetSize() * GetDepth();
-}
-
-inline std::size_t Image::GetDepth() const
-{
-  return m_bytes_per_pixel;
-}
-
-inline std::uint32_t* Image::GetData() const
-{
-  return m_pixels.mp_data;
-}
--- APiR/Image/src/Image.cpp
+++ APiR/Image/src/Image.cpp
@@ -1,6 +1,6 @@
 #include "Image/Image.h"
 
-Image::Image(std::size_t i_width, std::size_t i_height, std::uint32_t /*i_default_color*/)
+Image::Image(const std::size_t i_width, const std::size_t i_height, std::uint32_t /*i_default_color*/)
   : m_width(i_width)
   , m_height(i_height)
   , m_size(i_width * i_height)
@@ -14,14 +14,14 @@ Image::~Image()
   MemoryManager::clean(m_pixels);
 }
 
-void Image::SetPixelRGBA(std::size_t i_x,
-                         std::size_t i_y,
-                         uint8_t i_red,
-                         uint8_t i_green,
-                         uint8_t i_blue,
-                         uint8_t i_alpha)
+void Image::SetPixelRGBA(const std::size_t i_x,
+                         const std::size_t i_y,
+                         const uint8_t i_red,
+                         const uint8_t i_green,
+                         const uint8_t i_blue,
+                         const uint8_t i_alpha)
 {
-  auto rgba_data = reinterpret_cast<uint8_t*>(&m_pixels.mp_data[_ID(i_x, i_y)]);
+  auto* rgba_data = reinterpret_cast<uint8_t*>(&m_pixels.mp_data[_ID(i_x, i_y)]);
   rgba_data[0] = i_red;
   rgba_data[1] = i_green;
   rgba_data[2] = i_blue;
@@ -31,4 +31,63 @@ void Image::SetPixelRGBA(std::size_t i_x,
 uint8_t* Image::GetRGBAData() const
 {
   return reinterpret_cast<uint8_t*>(m_pixels.mp_data);
+}
+
+std::size_t Image::_ID(const std::size_t i_x, const std::size_t i_y) const
+{
+  return i_y * m_width + i_x;
+}
+
+std::uint32_t Image::GetPixel(const std::size_t i_x, const std::size_t i_y) const
+{
+  return m_pixels.mp_data[_ID(i_x, i_y)];
+}
+
+void Image::GetPixelRGBA(const std::size_t i_x,
+                         const std::size_t i_y,
+                         uint8_t& o_red,
+                         uint8_t& o_green,
+                         uint8_t& o_blue,
+                         uint8_t& o_alpha) const
+{
+  auto rgba_data = reinterpret_cast<const uint8_t*>(&m_pixels.mp_data[_ID(i_x, i_y)]);
+  o_red = rgba_data[0];
+  o_green = rgba_data[1];
+  o_blue = rgba_data[2];
+  o_alpha = rgba_data[3];
+}
+
+void Image::SetPixel(const std::size_t i_x, const std::size_t i_y, const std::uint32_t i_color)
+{
+  m_pixels.mp_data[_ID(i_x, i_y)] = i_color;
+}
+
+std::size_t Image::GetWidth() const
+{
+  return m_width;
+}
+
+std::size_t Image::GetHeight() const
+{
+  return m_height;
+}
+
+std::size_t Image::GetSize() const
+{
+  return m_size;
+}
+
+std::size_t Image::GetBytesCount() const
+{
+  return GetSize() * GetDepth();
+}
+
+std::size_t Image::GetDepth() const
+{
+  return m_bytes_per_pixel;
+}
+
+std::uint32_t* Image::GetData() const
+{
+  return m_pixels.mp_data;
 }
