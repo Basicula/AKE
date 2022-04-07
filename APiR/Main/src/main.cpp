78c87d2f9c9314a7bcefe0819733dfb0dfc9f525
Basicula <maksim.basum@gmail.com>
2022-04-07 19:35:43 +0300

    Move fluid example to separate file

--- APiR/Main/src/main.cpp
+++ APiR/Main/src/main.cpp
@@ -55,45 +55,6 @@
 #include <chrono>
 #include <iostream>
 
-void test_fluid()
-{
-  const std::size_t width = 640;
-  const std::size_t height = 480;
-
-  Scene scene("Fluid test");
-  Image image(width, height, 0xffaaaaaa);
-
-  scene.AddCamera(
-    new SimpleCamera(
-      Vector3d(0, 5, 5), Vector3d(0, -1, -1), Vector3d(0, sqrt(2) / 2, -sqrt(2) / 2), 75, width * 1.0 / height),
-    true);
-  scene.AddLight(new SpotLight(Vector3d(0, 10, 0)));
-  auto fluid = new Fluid(48);
-  scene.AddObject(fluid);
-
-  CPURayTracer renderer;
-  renderer.SetOutputImage(&image);
-  renderer.SetScene(&scene);
-  auto update_func = [&]() {
-    renderer.Render();
-    fluid->Update();
-  };
-#ifdef _DEBUG
-  for (int i = 0; i < 5; ++i) {
-    std::cout << i << std::endl;
-    auto start = std::chrono::system_clock::now();
-    update_func();
-    auto end = std::chrono::system_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
-  }
-#else
-  GLUTWindow window(width, height, "Fluid demo");
-  window.SetImageSource(&image);
-  window.SetUpdateFunction(update_func);
-  window.Open();
-#endif
-}
-
 void test_scene()
 {
   const std::size_t width = 800;
@@ -118,11 +79,11 @@ void test_scene()
 
 void test_scene_2d()
 {
-  constexpr std::size_t width = 800;
-  constexpr std::size_t height = 800;
+  constexpr std::size_t width = 400;
+  constexpr std::size_t height = 400;
   
   Scene2D scene("Test 2D scene");
-  auto unif_rand = []() { return 2.0 * (static_cast<double>(rand()) / RAND_MAX) - 1.0; };
+  auto unif_rand = []() { return (static_cast<double>(rand()) / RAND_MAX) - 0.5; };
   for (std::size_t circle_id = 0; circle_id < 10; ++circle_id) {
     const auto x = unif_rand();
     const auto y = unif_rand();
