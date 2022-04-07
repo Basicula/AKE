1e851f71c9edd2ed1aa7f40a8f2c8e0f9bf7492e
Basicula <maksim.basum@gmail.com>
2022-04-07 19:40:57 +0300

    Move 3d scene example to separate file

--- APiR/Main/src/main.cpp
+++ APiR/Main/src/main.cpp
@@ -55,28 +55,6 @@
 #include <chrono>
 #include <iostream>
 
-void test_scene()
-{
-  const std::size_t width = 800;
-  const std::size_t height = 600;
-
-  Scene scene = ExampleScene::RandomSpheres(20);
-
-  Image image(width, height);
-  CPURayTracer renderer;
-  renderer.SetOutputImage(&image);
-  renderer.SetScene(&scene);
-
-  auto update_func = [&]() { renderer.Render(); };
-  // GLUTWindow window(width, height, scene.GetName());
-  GLFWWindow window(width, height, scene.GetName());
-  window.SetImageSource(&image);
-  window.SetUpdateFunction(update_func);
-  window.SetEventListner(new SimpleCameraController(scene.GetActiveCamera()));
-  window.SetGUIView(new GLFWDebugGUIView(window.GetOpenGLWindow()));
-  window.Open();
-}
-
 void test_scene_2d()
 {
   constexpr std::size_t width = 400;
