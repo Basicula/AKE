f587af6dc70a23a339ffd3baae29aba01e62c146
Basicula <maksim.basum@gmail.com>
2022-04-09 13:32:36 +0300

    Clean examples main.cpp

--- APiR/Main/src/main.cpp
+++ APiR/Main/src/main.cpp
@@ -1,13 +1,3 @@
-#include "Common/ThreadPool.h"
-
-#ifdef ENABLED_CUDA
-#include "CUDACore/CUDAUtils.h"
-#include "CUDACore/KernelHandler.h"
-
-#include <Memory/device_ptr.h>
-#include <Memory/managed_ptr.h>
-#endif
-
 #include "Main/2DSceneExample.h"
 #include "Main/3DSceneExample.h"
 
