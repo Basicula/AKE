f1d2bf548dc64045aabc3837f05525b86f271a9c
Basicula <maksim.basum@gmail.com>
2022-04-12 21:02:40 +0300

    Revert "Simplify object class"
    This reverts commit 0899bc09b0d25461b75dfee0742841219242b701.

--- APiR/World.2D/include/World.2D/Object2D.h
+++ APiR/World.2D/include/World.2D/Object2D.h
@@ -5,9 +5,24 @@
 
 #include <memory>
 
-struct Object2D
+class Object2D
 {
+public:
+  template <class ShapeType, class... Args>
+  void InitShape(Args&&... i_args);
+  [[nodiscard]] const Shape2D& GetShape() const;
+
+  template <class DrawerType, class... Args>
+  void InitDrawer(Args&&... i_args);
+  [[nodiscard]] const Drawer& GetDrawer() const;
+
+  [[nodiscard]] const Transformation2D& GetTransformation() const;
+  [[nodiscard]] Transformation2D& GetTransformation();
+
+private:
   std::unique_ptr<Shape2D> mp_shape;
   std::unique_ptr<Drawer> mp_drawer;
   Transformation2D m_transformation;
 };
+
+#include "impl/Object2DImpl.h"
