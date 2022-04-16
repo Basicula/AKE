e5b7325a223b20375d7ef1032dfd9975514a064c
Basicula <maksim.basum@gmail.com>
2022-04-16 17:39:53 +0300

    Refactor examples

--- AKE/Main/include/Main/Example2D.h
+++ AKE/Main/include/Main/Example2D.h
@@ -1,10 +1,15 @@
 #pragma once
+
 #include <cstddef>
 
-namespace Scene2DExamples {
+namespace Example2D {
   void Circles(std::size_t i_window_width, std::size_t i_window_height, std::size_t i_circles_count);
   void Rectangles(std::size_t i_window_width, std::size_t i_window_height, std::size_t i_rectangles_count);
   void RotatedRectangles(std::size_t i_window_width, std::size_t i_window_height, std::size_t i_rectangles_count);
   void RotatedTriangles(std::size_t i_window_width, std::size_t i_window_height, std::size_t i_triangles_count);
-  void CollisionDetectionExample(std::size_t i_window_width, std::size_t i_window_height, std::size_t i_objects_count);
+
+
+  void CollisionDetection(const std::size_t i_window_width,
+                          const std::size_t i_window_height,
+                          const std::size_t i_objects_count);
 }
--- AKE/Main/src/main.cpp
+++ AKE/Main/src/main.cpp
@@ -1,11 +1,11 @@
-#include "Main/2DSceneExample.h"
+#include "Main/Example2D.h"
 #include "Main/3DSceneExample.h"
 #include "Main/FluidExample.h"
 #include "Main/FractalExample.h"
 
 int main()
 {
-  Scene2DExamples::CollisionDetectionExample(800, 800, 10);
+  Example2D::CollisionDetection(800, 800, 10);
   // SceneExample3D();
   // FluidExample();
   // FractalExample();
